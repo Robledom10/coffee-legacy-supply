a54b96c00c9a9fa3db842904c7757dca4bde8ec9
miguel robledo <stmiguelangelgarciarobledo@gmail.com>
2025-06-19 15:08:22 -0500

    Iniico del frontend

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -7,5 +7,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
