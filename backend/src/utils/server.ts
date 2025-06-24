aa96cf0f5317492954006a1c3f577d109269eee1
miguel robledo <stmiguelangelgarciarobledo@gmail.com>
2025-06-24 16:34:46 -0500

    Organizacion del backend

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -1,5 +1,5 @@
 import http from 'http';
-import { router } from './routes/router';
+import { router } from '../routes/router';
 
 const server = http.createServer((req, res) => {
   // Cabeceras CORS
