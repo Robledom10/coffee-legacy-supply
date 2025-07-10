62cc1ebc249071964f1be41c5e65810aeb1f2f15
miguel robledo <stmiguelangelgarciarobledo@gmail.com>
2025-07-09 23:24:45 -0500

    Entrega Proyecto

--- frontend/public/js/utils/crearElementoCarrito.js
+++ frontend/public/js/utils/crearElementoCarrito.js
@@ -1,11 +1,15 @@
-// utils/crearElementoProducto.js
 export function crearElementoProducto({ id, nombre, precio, imagen_url }) {
-  const div = document.createElement('div');
-  div.classList.add('producto');
+  const div = document.createElement("div");
+  div.classList.add("producto");
+
   div.innerHTML = `
-    <h3>${nombre}</h3>
-    <p>Precio: $${precio}</p>
     <img src="${imagen_url}" alt="${nombre}" class="producto-imagen">
+
+    <div class="info">
+      <h3>${nombre}</h3>
+      <p class="precio">$${precio.toLocaleString("es-CO")}</p>
+    </div>
+
     <button 
       class="btn-agregar" 
       data-id="${id}" 
@@ -15,5 +19,6 @@ export function crearElementoProducto({ id, nombre, precio, imagen_url }) {
       Agregar al carrito
     </button>
   `;
+
   return div;
 }
